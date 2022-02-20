11a83393a3122fe3d4eb807545ecfa1e0dc54027
Ischa Abraham <contact@ischa.dev>
2022-02-21 00:21:28 +0100

    clean up code by removing extraneous option

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tokio::io::{self, BufReader, AsyncBufReadExt};
-use tokio::sync::watch;
-use tokio_stream::wrappers::WatchStream;
+use tokio::sync::watch::{channel, Receiver};
+use tokio_stream::wrappers::WatchStream as StreamWrapper;
 
 use futures_util::StreamExt;
 
@@ -17,19 +17,15 @@ struct Args {
 }
 
 #[actix_web::get("/")]
-async fn stream(data: Data<watch::Receiver<Option<String>>>) -> impl Responder {
+async fn stream(data: Data<Receiver<String>>) -> impl Responder {
     // get the receiver from the Arc and turn it into an async stream
-    let rx = WatchStream::new((*data.into_inner()).clone());
+    let rx = StreamWrapper::new((*data.into_inner()).clone());
 
     // stream the data to the requester
     HttpResponse::Ok()
         .content_type("text/event_stream")
         .no_chunking(u64::MAX)
-        .streaming(rx.map(|data| match data {
-            // map the data to valid input/output for the connection
-            Some(data) => Ok(data.into()),
-            _ => Err(std::io::Error::new(std::io::ErrorKind::BrokenPipe, "Error")),
-        }))
+        .streaming::<_, std::io::Error>(rx.map(|data| Ok(data.into())))
 }
 
 #[tokio::main]
@@ -38,21 +34,19 @@ async fn main() -> std::io::Result<()> {
     let args = Args::parse();
 
     // create a channel, capable of handling multiple readers
-    let (tx, rx) = watch::channel::<Option<String>>(Some(String::from("\n")));
+    let (tx, rx) = channel::<String>(String::from("\n"));
 
     tokio::spawn(async move {
         let mut stdin = BufReader::new(io::stdin()).lines();
 
         // continually read lines from standard input
-        while let Some(mut line) = stdin.next_line().await.unwrap() {
-            line = line + "\n";
+        while let Some(mut block) = stdin.next_line().await.unwrap() {
+            block = block + "\n";
 
             // send the line into the receiver
-            tx.send(Some(line.into())).unwrap()
+            tx.send(block).ok();
         }
     });
-
-    println!("{}", args.address);
 
     // setup the stream to the http server
     let server = HttpServer::new(move ||
@@ -63,7 +57,6 @@ async fn main() -> std::io::Result<()> {
 
     // actually connect and run the web server, depending on the address
     if args.address.starts_with("unix:") {
-        println!("{}", &args.address[5..]);
         server.bind_uds(&args.address[5..])
     } else {
         server.bind(args.address)
