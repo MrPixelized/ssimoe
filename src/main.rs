c0771152302857887f319d0212ed92c096b5508f
Ischa Abraham <contact@ischa.dev>
2022-02-23 18:57:19 +0100

    get rid of commondata, actix

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,60 @@
 use std::sync::Arc;
 
-use tokio::io::{self, BufReader, AsyncBufReadExt};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{self, BufReader, AsyncBufReadExt, AsyncWriteExt};
 use tokio::sync::broadcast::{channel, Sender};
 use tokio_stream::wrappers::BroadcastStream as StreamWrapper;
-use tokio_stream::wrappers::errors::BroadcastStreamRecvError;
-
-use actix_web::{App, HttpServer, Responder, HttpResponse};
-use actix_web::web::{Data, Bytes};
+use tokio_stream::StreamExt;
 
 use clap::Parser;
 
-#[derive(Parser, Clone)]
+#[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Address to run the HTTP server on
     address: String,
-
-    /// Content type
-    #[clap(short, long, default_value = "text/event_stream")]
-    content_type: String,
 }
 
-struct CommonData {
-    args: Arc<Args>,
-    sender: Arc<Sender<Bytes>>,
+async fn handle_incoming(mut connection: TcpStream,
+                         mut stdin: StreamWrapper<String>,
+                         _args: Arc<Args>) -> io::Result<()> {
+    while let Some(Ok(block)) = stdin.next().await {
+        connection.write_all(block.as_bytes()).await?;
+    }
+
+    Err(std::io::ErrorKind::BrokenPipe.into())
 }
 
-#[actix_web::get("/")]
-async fn stream(data: Data<CommonData>) -> impl Responder {
-    // get the receiver from the Arc and turn it into an async stream
-    let rx = StreamWrapper::new(data.sender.subscribe());
+async fn stream_stdin_to(sender: Sender<String>) {
+    let mut stdin = BufReader::new(io::stdin()).lines();
 
-    // stream the data to the requester
-    HttpResponse::Ok()
-        // use the stored content type
-        .content_type(data.args.content_type.clone())
-        .no_chunking(u64::MAX)
-        .streaming::<_, BroadcastStreamRecvError>(rx)
+    while let Some(mut block) = stdin.next_line().await.unwrap() {
+        block = block + "\n";
+        sender.send(block.into()).ok();
+    }
 }
 
 #[tokio::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
     // parse arguments
-    let args = Args::parse();
-    let data_args = Arc::new(args.clone());
+    let args = Arc::new(Args::parse());
 
     // create a channel, capable of handling multiple readers
-    let (sender, _) = channel::<Bytes>(16);
+    let (sender, _) = channel::<String>(16);
 
-    // make sure one end can be sent to connecting tasks, and one is kept here
-    // for writing
-    let sender = Arc::new(sender);
-    let data_sender = Arc::clone(&sender);
+    // spawn a task to stream stdin to this channel
+    tokio::spawn(stream_stdin_to(sender.clone()));
 
-    // start streaming stdin
-    tokio::spawn(async move {
-        let mut stdin = BufReader::new(io::stdin()).lines();
+    // start receiving and handling requests
+    let listener = TcpListener::bind(args.address.clone()).await?;
 
-        while let Some(mut block) = stdin.next_line().await.unwrap() {
-            block = block + "\n";
-            sender.send(block.into()).ok();
-        }
-    });
+    while let Ok((connection, _)) = listener.accept().await {
+        // setup a new receiving channel
+        let stream = StreamWrapper::new(sender.subscribe());
 
-    // setup the stream to the http server
-    let server = HttpServer::new(move ||
-            App::new()
-                .app_data(Data::new(CommonData {
-                    args: Arc::clone(&data_args),
-                    sender: Arc::clone(&data_sender)
-                }))
-                .service(stream)
-        );
+        // spawn a new task to forward it to the incoming connection
+        tokio::spawn(handle_incoming(connection, stream, Arc::clone(&args)));
+    }
 
-    // actually connect and run the web server, depending on the address
-    if args.address.starts_with("unix:") {
-        server.bind_uds(&args.address[5..])
-    } else {
-        server.bind(args.address)
-    }?
-    .run()
-    .await
+    Ok(())
 }
